7d07284621899c37c504ed43909f25b615a2c663
Thibault Longuepee <thibault.longuepee@epitech.eu>
2025-10-19 11:14:09 +0200

    feat(FloatingListItem): add activation prop

--- frontend/src/components/FloatingListItem.tsx
+++ frontend/src/components/FloatingListItem.tsx
@@ -3,11 +3,12 @@ import styles from './FloatingListItem.module.css'
 type Props = {
     value: string
     onClick?: () => void
+    active?: boolean
 }
 
-export default function FloatingListItem({ value, onClick }: Props) {
+export default function FloatingListItem({ value, onClick, active }: Props) {
     return (
-        <div className={styles.item} onClick={onClick}>
+        <div className={active ? styles.active : styles.inactive} onClick={onClick}>
             {value}
         </div>
     )
