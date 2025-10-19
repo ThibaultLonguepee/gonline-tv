975e640e11ff8ffa3261784fdee3790334e58f80
Thibault Longuepee <thibault.longuepee@epitech.eu>
2025-10-19 19:21:16 +0200

    feat(LiveStreamsView): listing and details of live streams per category

--- frontend/src/pages/home/LiveStreamsView.tsx
+++ frontend/src/pages/home/LiveStreamsView.tsx
@@ -1,13 +1,20 @@
 import { useEffect, useState } from "react";
-import { ListLiveCategories } from '../../../wailsjs/go/main/App'
+import { GetLiveStreamLink, ListLiveCategories, ListLiveStreams } from '../../../wailsjs/go/main/App'
 import FloatingListItem from "../../components/FloatingListItem";
 import FloatingList from "../../components/FloatingList";
 import { models } from "../../../wailsjs/go/models";
+import styles from "./LiveStreamsView.module.css"
 
 export default function LiveStreamsView() {
 
     const [categories, setCategories] = useState<models.Category[]>([])
     const [selectedCategory, setSelectedCategory] = useState<models.Category>()
+
+    const [streams, setStreams] = useState<models.LiveStream[]>([])
+    const [selectedStream, setSelectedStream] = useState<models.LiveStream>()
+    const [streamUrl, setStreamUrl] = useState("")
+    const [showUrl, setShowUrl] = useState(false)
+    const [status, setStatus] = useState("")
 
     useEffect(() => {
         ListLiveCategories()
@@ -15,13 +22,66 @@ export default function LiveStreamsView() {
             .catch()
     }, [])
 
+    function selectCategory(category: models.Category) {
+        setSelectedCategory(category)
+        ListLiveStreams(category.Id)
+            .then(result => setStreams(result))
+            .catch()
+    }
+    
+    function selectStream(stream: models.LiveStream) {
+        setSelectedStream(stream)
+        setShowUrl(false)
+        setStatus("")
+        GetLiveStreamLink(stream.Id)
+            .then(result => setStreamUrl(result))
+            .catch()
+    }
+
+    function copyUrl() {
+        navigator.clipboard.writeText(streamUrl)
+        setStatus("✅ Copied URL to the Clipboard!")
+    }
+    
+    function toggleShowUrl() {
+        setShowUrl(showUrl => (!showUrl))
+    }
+
     return (
-        <FloatingList>
+        <>
+            <FloatingList>
+                {
+                    categories?.map((category, _) => (
+                        <FloatingListItem value={category.Name} onClick={() => selectCategory(category)} active={category.Id === selectedCategory?.Id} />
+                    ))
+                }
+            </FloatingList>
+            <FloatingList>
+                {
+                    streams?.map((stream, _) => (
+                        <FloatingListItem value={stream.Name} onClick={() => selectStream(stream)} active={stream.Id === selectedStream?.Id} />
+                    ))
+                }
+            </FloatingList>
             {
-                categories?.map((category, _) => (
-                    <FloatingListItem value={category.Name} onClick={() => setSelectedCategory(category)} active={category.Id === selectedCategory?.Id} />
-                ))
+                selectedStream != undefined && <div className={styles.details}>
+                    <div className={styles.top}>
+                        <img src={selectedStream?.Icon}></img>
+                        <span className={styles.title}>{selectedStream.Name}</span>
+                    </div>
+                    <div className={styles.url}>
+                        <span className={styles.label}>URL</span>
+                        <div className={styles.urlInput}>
+                            <input type={showUrl ? 'text' : 'password'} value={streamUrl} size={30} readOnly={true} />
+                            <button className={styles.show} onClick={() => toggleShowUrl()}>👀</button>
+                            <button className={styles.copy} onClick={() => copyUrl()}>Copy</button>
+                        </div>
+                        {
+                            status != "" && <span className={styles.status}>{status}</span>
+                        }
+                    </div>
+                </div>
             }
-        </FloatingList>
+        </>
     )
 }
