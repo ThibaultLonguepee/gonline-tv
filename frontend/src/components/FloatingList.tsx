20bdf22b5c2dff74d9fe0b8cc018d7229c49df1d
Thibault Longuepee <thibault.longuepee@epitech.eu>
2025-10-27 15:12:15 +0100

    feat(FloatingList): add width modifier for more static look

--- frontend/src/components/FloatingList.tsx
+++ frontend/src/components/FloatingList.tsx
@@ -1,13 +1,13 @@
 import styles from './FloatingList.module.css'
 
-export default function FloatingList({ children }: { children: React.ReactElement[] }) {
+export default function FloatingList({ width = 350, children }: { width?: number, children: React.ReactElement[] }) {
     
     if (children.length === 0) {
         return (<></>)
     }
 
     return (
-        <div className={styles.list}>
+        <div className={styles.list} style={{minWidth: width, maxWidth: width}}>
             {children}
         </div>
     )
